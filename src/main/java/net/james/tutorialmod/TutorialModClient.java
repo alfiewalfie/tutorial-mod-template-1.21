67905c9aa8b7d79e4545fabbe63f4921a8e71354
James Burton <jamesburton@Jamess-iMac.local>
2024-08-09 11:08:50 +0100

    Initial commit

---
+++ src/main/java/net/james/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.james.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
